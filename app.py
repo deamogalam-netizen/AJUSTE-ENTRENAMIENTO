aa35a7802bd5b0fd4dd3f02d3a59471efd680e56
deamogalam-netizen <deamogalam@gmail.com>
2026-09-04 18:53:47 +0200

    Update app.py

--- app.py
+++ app.py
@@ -63,7 +63,7 @@ def evaluar_readiness():
     with col7:
         ayer_rojo = st.checkbox("¿Ayer hiciste un entreno ROJO (Series Z5)?")
     with col8:
-        dos_amarillos = st.checkbox("¿Llevas 2 días consecutivos de entrenamiento AMARILLO (Z3-Z4)?")
+        dos_amarillos = st.checkbox("¿Llevas 2 días consecutivos de entrenamiento AMARILLO o AMARILLO-ROJO?")
 
     if st.button("Generar Semáforo Diario"):
         irs_empeora = sensacion_general == "Peor"
